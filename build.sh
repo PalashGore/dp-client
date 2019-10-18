6a7bb02bd17f4e041057079e5d89b695d95cd3fb
pgore <PalashGore@users.noreply.github.com>
2019-10-18 09:07:33 -0400

    Tagged image

--- build.sh
+++ build.sh
@@ -9,5 +9,5 @@ fi
 
 echo "Building docker image with tag :$VERSION"
 cd C:/projects/deployment/client-deployment
-docker build -t client-image:$VERSION .
+docker build -t gcr.io/cloudpark-e6cc4/client-image:$VERSION .
 gcloud docker -- push gcr.io/cloudpark-e6cc4/client-image:$VERSION
